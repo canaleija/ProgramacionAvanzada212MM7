6343f2439b729a546505430b88de5d0a0bc40c91
working <working@DESKTOP-17KGHMK>
2021-03-19 12:22:40 -0600

    19/03/21 - primer version del proyecto de PA 2mm7

---
+++ src/programacionavanzada212mm7/ProgramacionAvanzada212MM7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionavanzada212mm7;
+
+/**
+ *
+ * @author working
+ */
+public class ProgramacionAvanzada212MM7 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

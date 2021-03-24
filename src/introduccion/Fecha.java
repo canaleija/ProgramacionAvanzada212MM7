92e6e3cac3220c94bdbcfe4144c5d0c9f6117707
working <working@DESKTOP-17KGHMK>
2021-03-24 13:01:24 -0600

    24/03/21 - creamos la clase fecha

---
+++ src/introduccion/Fecha.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introduccion;
+
+/**
+ *
+ * @author working
+ */// las llaves me definen el contexto de la clase
+public class Fecha {
+    
+    // metodo constructor por defecto
+    public Fecha(){
+    // nada
+    }
+    // variables globales (contenedor o espacio de memoria)
+    public int dia;
+    public int mes;
+    public int anio;
+    // el objeto existe cuando el programa se ejecuta siempre y cuando
+    // yo genere la instancia del objeto
+}
--- src/programacionavanzada212mm7/ProgramacionAvanzada212MM7.java
+++ src/programacionavanzada212mm7/ProgramacionAvanzada212MM7.java
@@ -5,17 +5,30 @@
  */
 package programacionavanzada212mm7;
 
+import introduccion.Fecha;
 /**
  *
  * @author working
  */
 public class ProgramacionAvanzada212MM7 {
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //
         System.out.print("Hola mundo!");
+        // variable tipo: Fecha id: 
+        Fecha hoy = new Fecha();
+        hoy.dia = 24;
+        hoy.mes=3;
+        hoy.anio=2021;
+        Fecha manana = new Fecha();
+        System.out.println();
+        // Todas las clases se identifica la 1er letra con mayúscula
+        // Las clases ademas que permiten crear plantillas para generar objetos 
+        // tambien me ayudan a crear nuevos tipos de datos
     }
     
 }
